ff99b7b439e059bcd5f4a62f574a2529df702796
Louis-Michel Raynauld <louismichel@pweb.ca>
2013-07-15 18:19:20 -0400

    Added plugin basic requirements checks

--- src/WPCore/WPplugin.php
+++ src/WPCore/WPplugin.php
@@ -20,6 +20,13 @@ abstract class WPplugin extends WPfeature
 
   static $instance;
 
+  protected $reqPHPVersion  = '5.3.0';
+  protected $reqWPVersion   = '3.0.0';
+  protected $langDir        = '/lang';
+  protected $errors  = array();
+
+  protected $reqPlugins = array();
+
   public function __construct($file, $name, $slug)
   {
     parent::__construct($name, $slug);
@@ -30,8 +37,74 @@ abstract class WPplugin extends WPfeature
     register_activation_hook( $file, array($this, 'install') );
   }
 
+  public function getLangDir()
+  {
+    return $this->langDir;
+  }
+
+  public function getErrors()
+  {
+    return $this->errors;
+  }
+
+  public function setLangDir($langDir)
+  {
+    $this->langDir = $langDir;
+  }
+
+  public function setReqPhpVersion($reqPHPVersion)
+  {
+    $this->reqPHPVersion = $reqPHPVersion;
+  }
+
+  public function setReqWpVersion($reqWPVersion)
+  {
+    $this->reqWPVersion = $reqWPVersion;
+  }
+
+  public function requires($pluginMainFile)
+  {
+    $this->reqPlugins[] = $pluginMainFile;
+  }
+
+  //TODO hook this on any plugin desactivation
+  public function checkPluginsRequirements()
+  {
+    $return = true;
+
+    //Custom plugins requirements
+    foreach($this->reqPlugins as $pluginMainFile)
+    {
+      if ( !in_array( $pluginMainFile, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) )
+      {
+        $this->errors[] = $pluginMainFile;
+        $return = false;
+      }
+    }
+
+    return $return;
+  }
+
+  //Errors & requirements
+  public function checkServerRequirements()
+  {
+    $return = true;
+    //PHP version requirements
+    if (version_compare(PHP_VERSION, $this->reqPHPVersion, '<'))
+    {
+      $this->errors[] = 'PHP';
+      $return = false;
+    }
+
+    //WP version requirements
+    if( version_compare( $GLOBALS['wp_version'], $this->reqWPVersion, '<' ) )
+    {
+      $this->errors[] = 'WP';
+      $return = false;
+    }
+
+    return $return;
+  }
   abstract public function install();
-
-
 
 }
