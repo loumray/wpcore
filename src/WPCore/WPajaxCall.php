aa6c60ea7a90fbc8a60371d0f053a92fd4e34a03
Louis-Michel Raynauld <louismichel@pweb.ca>
2013-09-28 12:22:29 -0400

    Improve Ajax call to better integrate security checks and ease usage:

--- src/WPCore/WPajaxCall.php
+++ src/WPCore/WPajaxCall.php
@@ -22,16 +22,49 @@ abstract class WPajaxCall implements WPhook
   protected $admin;
   protected $mustBeLoggedIn;
   protected $jsvar;
+  protected $nonceSlug;
 
   protected $currentUser;
 
-  public function __construct($call_slug, $js_handle, $admin = false, $mustBeLoggedIn = false)
+  protected $formData = array();
+
+  public function __construct(
+      $call_slug, 
+      $js_handle, 
+      $admin = false, 
+      $mustBeLoggedIn = false, 
+      $nonceSlug = ""
+  )
   {
-    $this->slug      = $call_slug;
+    $this->slug     = $call_slug;
     $this->jsHandle = $js_handle;
-    $this->admin = $admin;
+    $this->admin    = $admin;
     $this->mustBeLoggedIn = $mustBeLoggedIn || $admin;
-    $this->jsvar = $this->slug.'_vars';
+
+    $this->jsvar = $this->slug.'_params';
+    $this->nonceSlug = $nonceSlug;
+    if(empty($this->nonceSlug))
+    {
+      $this->nonceSlug = $this->slug.'-action';
+    }
+  }
+
+  abstract public function callback($data);
+
+  public function safeCallback()
+  {
+    $this->verify();
+
+    $data = array();
+    if(!empty($_POST['data']))
+    {
+      foreach($_POST['data'] as $info)
+      {
+        $data[$info['name']] = $info['value'];
+      }
+    }
+
+    return $this->callback($data);
   }
 
   public function getActionSlug()
@@ -44,14 +77,24 @@ abstract class WPajaxCall implements WPhook
     return $this->jsvar;
   }
 
-  abstract public function init();
-  abstract public function callback();
+  public function init()
+  {
+
+    $params = array(
+        'ajax_url' => admin_url( 'admin-ajax.php' ),
+        'nonce'    => wp_create_nonce( $this->nonceSlug )
+    );
+
+    wp_localize_script( $this->jsHandle, $this->jsvar, $params );
+  }
 
   /*
    * If logged in needed  sets current user or die if no user are logged
    */
   protected function verify()
   {
+    check_ajax_referer( $this->nonceSlug, 'security' );
+
     if($this->mustBeLoggedIn === true)
     {
       $this->setCurrentUser();
@@ -81,11 +124,11 @@ abstract class WPajaxCall implements WPhook
     {
       add_action( 'wp_enqueue_scripts', array($this,'init'),10000 );
     }
-    add_action( 'wp_ajax_'.$this->slug, array($this,'callback') );
+    add_action( 'wp_ajax_'.$this->slug, array($this,'safeCallback') );
 
     if(!$this->mustBeLoggedIn)
     {
-      add_action('wp_ajax_nopriv_'.$this->slug, array($this,'callback'));
+      add_action('wp_ajax_nopriv_'.$this->slug, array($this,'safeCallback'));
     }
   }
 
@@ -100,11 +143,11 @@ abstract class WPajaxCall implements WPhook
       remove_action( 'wp_enqueue_scripts', array($this,'init') );
     }
 
-    remove_action('wp_ajax_'.$this->slug, array($this,'callback'));
+    remove_action('wp_ajax_'.$this->slug, array($this,'safeCallback'));
 
     if(!$this->mustBeLoggedIn)
     {
-      remove_action('wp_ajax_nopriv_'.$this->slug, array($this,'callback'));
+      remove_action('wp_ajax_nopriv_'.$this->slug, array($this,'safeCallback'));
     }
   }
 }
