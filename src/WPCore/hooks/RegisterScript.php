5ab6d22ddf445291ead4effc207eb820b141e9f8
Louis-Michel Raynauld <louismichel@pweb.ca>
2013-09-28 12:26:52 -0400

    Added register script hook

---
+++ src/WPCore/hooks/RegisterScript.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of WPCore project.
+ *
+ * (c) Louis-Michel Raynauld <louismichel@pweb.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WPCore\hooks;
+
+use WPCore\WPaction;
+use WPCore\WPscript;
+
+/**
+ * WP theme scripts hook
+ *
+ * @author Louis-Michel Raynauld <louismichel@pweb.ca>
+ */
+
+class RegisterScript extends WPaction
+{
+    protected $script;
+
+    public function __construct(WPscript $script)
+    {
+        $this->script = $script;
+
+        parent::__construct('wp_enqueue_scripts',5,1);
+    }
+
+    public function action()
+    {
+        $this->script->register();
+    }
+}
