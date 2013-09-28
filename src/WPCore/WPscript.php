71116f74bfb26537ca9c721f7ac5e3800cccaab5
Louis-Michel Raynauld <louismichel@pweb.ca>
2013-09-28 12:24:08 -0400

    Added register method

--- src/WPCore/WPscript.php
+++ src/WPCore/WPscript.php
@@ -18,31 +18,41 @@ namespace WPCore;
 
 class WPscript
 {
-  protected $handle;
-  protected $src       = "";
-  protected $deps      = array();
-  protected $ver       = false;
-  protected $in_footer  = true;
+    protected $handle;
+    protected $src       = "";
+    protected $deps      = array();
+    protected $ver       = false;
+    protected $in_footer  = true;
 
-  public function __construct($handle, $src = false, $deps = array(),$ver = false, $in_footer = true)
-  {
-    $this->handle    = $handle;
-    $this->src       = $src;
-    $this->deps      = $deps;
-    $this->ver       = $ver;
-    $this->in_footer = $in_footer;
-  }
+    public function __construct($handle, $src = false, $deps = array(),$ver = false, $in_footer = true)
+    {
+        $this->handle    = $handle;
+        $this->src       = $src;
+        $this->deps      = $deps;
+        $this->ver       = $ver;
+        $this->in_footer = $in_footer;
+    }
 
+    public function enqueue()
+    {
+        wp_enqueue_script(
+            $this->handle,
+            $this->src,
+            $this->deps,
+            $this->ver,
+            $this->in_footer
+        );
+    }
 
-  public function enqueue()
-  {
-    wp_enqueue_script(
-                      $this->handle,
-                      $this->src,
-                      $this->deps,
-                      $this->ver,
-                      $this->in_footer
-                     );
-  }
+    public function register()
+    {
+        wp_register_script( 
+            $this->handle, 
+            $this->src, 
+            $this->deps, 
+            $this->ver, 
+            $this->in_footer 
+        );
+    }
 
 }
