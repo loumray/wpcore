e0c05a98d96cf93bc3b6e4d0f17fb70e5ee286a1
Louis-Michel Raynauld <louismichel@pweb.ca>
2013-09-28 12:26:14 -0400

    Addded WPoption support

---
+++ src/WPCore/WPoption.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * This file is part of WPCore project.
+ *
+ * (c) Louis-Michel Raynauld <louismichel@pweb.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WPCore;
+
+/**
+ * WP option
+ *
+ * @author Louis-Michel Raynauld <louismichel@pweb.ca>
+ */
+
+class WPoption
+{
+  protected $name;
+  protected $value;
+  protected $autoload = true;
+  protected $fetched  = false;
+
+  public function __construct($optionName)
+  {
+    $this->name = $optionName;
+  }
+
+  public function getAutoload() {
+      return $this->autoload;
+  }
+  
+  public function setAutoload($autoload) {
+      $this->autoload = $autoload;
+  
+      return $this;
+  }
+
+  public function getValue() {
+      if($this->fetched === false)
+      {
+          $this->fetch();
+      }
+      return $this->value;
+  }
+  
+  public function setValue($value) {
+      $this->value = $value;
+  
+      return $this;
+  }
+
+  public function fetch()
+  {
+    $this->value = get_option($this->name);
+    $this->fetched = true;
+  }
+  /*
+   * @return True if option has been deleted, false if not or if deletion failed.
+  */
+  public function delete()
+  {
+    return delete_option($this->name);
+  }
+
+  public function save()
+  {
+    if ( get_option( $this->name ) !== false ) {
+        return update_option( $this->name, $this->value );
+    } else {
+        // The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+        $autoload = ($this->autoload === true) ? 'yes' : 'no';
+        return add_option( $this->name, $this->value, null, $autoload );
+    }
+  }
+}
