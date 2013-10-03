4aed9483065c5252f29e4dc1b9fdd9a8a86433c2
Louis-Michel Raynauld <louismichel@pweb.ca>
2013-10-03 13:31:35 -0400

    Added getters and setters

--- src/WPCore/WPscript.php
+++ src/WPCore/WPscript.php
@@ -22,15 +22,15 @@ class WPscript
     protected $src       = "";
     protected $deps      = array();
     protected $ver       = false;
-    protected $in_footer  = true;
+    protected $inFooter  = true;
 
-    public function __construct($handle, $src = false, $deps = array(), $ver = false, $in_footer = true)
+    public function __construct($handle, $src = false, $deps = array(), $ver = false, $inFooter = true)
     {
         $this->handle    = $handle;
         $this->src       = $src;
         $this->deps      = $deps;
         $this->ver       = $ver;
-        $this->in_footer = $in_footer;
+        $this->inFooter = $inFooter;
     }
 
     public function enqueue()
@@ -40,7 +40,7 @@ class WPscript
             $this->src,
             $this->deps,
             $this->ver,
-            $this->in_footer
+            $this->inFooter
         );
     }
 
@@ -51,7 +51,127 @@ class WPscript
             $this->src,
             $this->deps,
             $this->ver,
-            $this->in_footer
+            $this->inFooter
         );
+    }
+
+    /**
+     * Gets the value of handle.
+     *
+     * @return mixed
+     */
+    public function getHandle()
+    {
+        return $this->handle;
+    }
+
+    /**
+     * Sets the value of handle.
+     *
+     * @param mixed $handle the handle
+     *
+     * @return self
+     */
+    public function setHandle($handle)
+    {
+        $this->handle = $handle;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of src.
+     *
+     * @return mixed
+     */
+    public function getSrc()
+    {
+        return $this->src;
+    }
+
+    /**
+     * Sets the value of src.
+     *
+     * @param mixed $src the src
+     *
+     * @return self
+     */
+    public function setSrc($src)
+    {
+        $this->src = $src;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of deps.
+     *
+     * @return mixed
+     */
+    public function getDeps()
+    {
+        return $this->deps;
+    }
+
+    /**
+     * Sets the value of deps.
+     *
+     * @param mixed $deps the deps
+     *
+     * @return self
+     */
+    public function setDeps($deps)
+    {
+        $this->deps = $deps;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of ver.
+     *
+     * @return mixed
+     */
+    public function getVer()
+    {
+        return $this->ver;
+    }
+
+    /**
+     * Sets the value of ver.
+     *
+     * @param mixed $ver the ver
+     *
+     * @return self
+     */
+    public function setVer($ver)
+    {
+        $this->ver = $ver;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of inFooter.
+     *
+     * @return mixed
+     */
+    public function getInFooter()
+    {
+        return $this->inFooter;
+    }
+
+    /**
+     * Sets the value of inFooter.
+     *
+     * @param mixed $inFooter the inFooter
+     *
+     * @return self
+     */
+    public function setInFooter($inFooter)
+    {
+        $this->inFooter = $inFooter;
+
+        return $this;
     }
 }
