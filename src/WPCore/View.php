564b34128ac387b030ee15810ee76f83aa009ea5
Louis-Michel Raynauld <louismichel@pweb.ca>
2014-01-30 18:20:56 -0500

    Added WP context to view file and allow template override via file in theme folder

--- src/WPCore/View.php
+++ src/WPCore/View.php
@@ -18,17 +18,23 @@ namespace WPCore;
 class View
 {
     protected $file;
+    protected $name;
     protected $data;
+
+    protected $allowOverride = false;
+    protected $overrideDir = '';
 
     public function __construct($file, $data = array())
     {
         $this->file = $file;
+        $this->name = basename($file);
         $this->data = $data;
     }
 
     public function setData($data)
     {
         $this->data = $data;
+        return $this;
     }
 
     public function getContent()
@@ -40,12 +46,35 @@ class View
         return $out;
     }
 
+    public function setAllowOverride($allow = true)
+    {
+        $this->allowOverride = $allow;
+        return $this;
+    }
+    public function setOverrideDir($subDir)
+    {
+        $this->overrideDir = $subDir.'/';
+        return $this;
+    }
+
     public function show()
     {
+        if (($this->allowOverride === true) && 
+            $override = locate_template($this->overrideDir.$this->name)
+        ) {
+            $this->file = $override;
+        } 
+
         if (file_exists($this->file)) {
+            //load WP context
+            global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+
+            if (is_array( $wp_query->query_vars)) {
+                extract( $wp_query->query_vars, EXTR_SKIP );
+            }
+            //load view data
             extract($this->data);
             include($this->file);
-
             return true;
         } else {
             throw new \Exception("File not found on ".$this->file);
