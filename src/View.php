c8b333081f1409cf03808004b4d3cfe801b24615
Louis-Michel Raynauld <louismichel@pweb.ca>
2014-06-05 16:11:28 -0400

    Code standard fix

--- src/View.php
+++ src/View.php
@@ -64,18 +64,18 @@ class View
 
     public function show()
     {
-        if (($this->allowOverride === true) && 
+        if (($this->allowOverride === true) &&
             $override = locate_template($this->overrideDir.$this->overrideName)
         ) {
             $this->file = $override;
-        } 
+        }
 
         if (file_exists($this->file)) {
             //load WP context
             global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
 
-            if (is_array( $wp_query->query_vars)) {
-                extract( $wp_query->query_vars, EXTR_SKIP );
+            if (is_array($wp_query->query_vars)) {
+                extract($wp_query->query_vars, EXTR_SKIP);
             }
             //load view data
             extract($this->data);
